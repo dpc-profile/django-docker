ccfa936c6e1bfe30c3592fc9a235fb7e62045563
Daniel Pereira <daniel.p@bpatechnologies.com>
2021-03-27 17:34:56 -0300

    Tentando corrigir css e imagens não encontradas

--- simplemooc/core/urls.py
+++ simplemooc/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'core'   # Em urls.py de outras paginas altere o appname para
                     # de acordo com a pasta acima. Ex: 'core' por causa
@@ -8,4 +10,4 @@ app_name = 'core'   # Em urls.py de outras paginas altere o appname para
 urlpatterns = [
     path('', views.home, name='home'),
     path('contato/', views.contato, name='contact'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
